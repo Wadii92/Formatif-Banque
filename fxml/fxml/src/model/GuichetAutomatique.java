1a15e2de9d28cd825538dcbaa75c984a0f1d2af6
2032659 <2032659@cegeplimoilou.ca>
2024-04-07 01:49:53 -0700

    Construire la classe GuichetAutomatique, ainsi ses methodes deposerArgent et retirerArgent. Ajout d'une methode trouverCompte

--- fxml/fxml/src/model/GuichetAutomatique.java
+++ fxml/fxml/src/model/GuichetAutomatique.java
@@ -3,7 +3,9 @@ package model;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.Alert;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -14,10 +16,21 @@ public final class GuichetAutomatique {
     /**
      * Creation d'une variable comptesList, qui va contenir une list de comptes non doublon
      */
-    HashSet<Comptes> comptesList;
+    private HashSet<Comptes> comptesList;
 
+
+
+    /**
+     * Le numéro de compte du compte bancaire.
+     */
     private final SimpleIntegerProperty numeroCompte = new SimpleIntegerProperty();
+    /**
+     * Le montant du dépôt effectué sur ce compte bancaire.
+     */
     private final SimpleDoubleProperty mtnDepot = new SimpleDoubleProperty();
+    /**
+     * Le montant du retrait effectué sur ce compte bancaire.
+     */
     private final SimpleDoubleProperty mtnRetrait = new SimpleDoubleProperty();
 
     /**
@@ -75,6 +88,68 @@ public final class GuichetAutomatique {
         this.comptesList = comptesList;
     }
 
+    /**
+     * Methode permet de deposer de l'argent dans le compte
+     * @param numeroCompte
+     */
+
+
+    public void deposerArgent(int numeroCompte){
+
+        Comptes comptes = trouverCompte(numeroCompte);
+        if (comptes != null){
+            comptes.deposer(mtnDepot.get());
+            System.out.println("Depot reussi");
+            System.out.println("Nouveau solde: " + comptes.getSolde());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Depot Reussi");
+            alert.setHeaderText("Depot");
+            alert.setContentText("Nouveau solde : " + comptes.getSolde());
+            alert.showAndWait();
+        }
+
+    }
+
+
+    /**
+     * Methode permet de retirer de l'argent dans le compte
+     * @param numeroCompte
+     */
+
+    public void retirerArgent(int numeroCompte){
+        Comptes comptes = trouverCompte(numeroCompte);
+        if (comptes != null){
+            comptes.retirer(mtnRetrait.get());
+
+            System.out.println("Retrait reussi");
+            System.out.println("Nouveau solde : " + comptes.getSolde());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Retrait effectue");
+            alert.setHeaderText("Retrait");
+            alert.setContentText("Nouveau solde : " + comptes.getSolde());
+            alert.showAndWait();
+        }
+
+
+    }
+
+
+    /**
+     * methode permet de trouver un compte dans une liste de compte
+     *
+     * @param numeroCompte
+     * @return compte
+     */
+
+    private Comptes trouverCompte(int numeroCompte) {
+        for(Comptes compte : comptesList){
+            if(compte.getNumCompte() == numeroCompte){
+                return compte;
+            }
+        }
+        return null;
+    }
+
 
     /**
      * Methode qui permet d'afficher une boite de dialogue avec un message d'aide
@@ -82,9 +157,7 @@ public final class GuichetAutomatique {
      */
     public String obtenirDeLAide() {
         String message= "";
-
-        message = "Numéro de téléphone : 418 000 0000";
-
+        message = "Numéro de téléphone du support : 418 000 0000";
         return message;
     }
 
